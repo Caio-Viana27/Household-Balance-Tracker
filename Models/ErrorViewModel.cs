bec5ae30a1258609c4f029bcac32887c342d4f1c
caioviana <caio.devwork@gmail.com>
2025-03-22 20:31:49 -0300

    Project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace household_balance.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
