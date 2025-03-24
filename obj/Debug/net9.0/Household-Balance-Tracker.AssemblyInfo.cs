4ecb93dd829003918c987dc10920069412c37b52
caioviana <caio.devwork@gmail.com>
2025-03-23 22:13:37 -0300

    age field added to person form

--- obj/Debug/net9.0/Household-Balance-Tracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/Household-Balance-Tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Household-Balance-Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed936b0c62ec882e92bc19e74d3d37403a6e173e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e79032f25ae5ae1b47c1e1da787033efa845df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Household-Balance-Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Household-Balance-Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
